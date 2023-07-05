187df7315b10a4677873923daa19d7d4ab9960dd
Bruce Simiyu <bruce14simiyu@gmail.com>
2023-07-05 10:12:58 +0300

    might fail to work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
 import {AiFillTwitterCircle, AiFillLinkedin, AiFillGithub} from 'react-icons/ai';
+import Image from "next/image"
+import profile from "../../public/profile.jpg"
 
 export default function Home() {
   return (
    <div>
     <main className="bg-white px-10">
       <section className="h-screen ">
-        <nav className="py-10 mb-1">
+        <nav className="py-4 mb-1">
           <h1 className='font-bold'><code>Bruce</code></h1>
         </nav>
         <div className="text-center">
           <h2 className='text-4xl py-2 text-teal-700 font-medium'>OBruce Simiyu</h2>
           <h3 className='py-2 font-medium'>Web Developer | Technical Writer</h3>
-          <p className='leading-8 py-3 text-gray-900'>A freelance web developer with passion for the frontend.
+          <p className='leading-6 py-3 text-gray-900'>A freelance web developer with passion for the frontend.
             Enjoys working with React, TypeScript and JavaScript technologies. 
             You will also find me using Tailwind CSS
           </p>
@@ -20,6 +22,10 @@ export default function Home() {
           <AiFillTwitterCircle />
           <AiFillLinkedin />
           <AiFillGithub />
+        </div>
+        <div className=''>
+          <Image src={profile} alt=""/>
+
         </div>
       </section>
     </main>
