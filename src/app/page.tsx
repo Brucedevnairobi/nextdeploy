78486ee57d675ea581cf3916efb2bf1f7bbc5dbd
Bruce Simiyu <bruce14simiyu@gmail.com>
2023-07-05 05:29:11 +0300

    Create a profile page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,29 @@
+import {AiFillTwitterCircle, AiFillLinkedin, AiFillGithub} from 'react-icons/ai';
+
 export default function Home() {
   return (
-    <main className="">
-      <h1 className="font-bold underline text-4xl text-center">Hello Kenya</h1>
-      <h2 className="font-bold text-3xl">hello world</h2>
+   <div>
+    <main className="bg-white px-10">
+      <section className="h-screen ">
+        <nav className="py-10 mb-1">
+          <h1>Bruce /></h1>
+        </nav>
+        <div className="text-center">
+          <h2 className='text-4xl py-2 text-teal-700 font-medium'>OBruce Simiyu</h2>
+          <h3 className='py-2 font-medium'>Web Developer | Technical Writer</h3>
+          <p className='leading-8 py-3 text-gray-900'>A freelance web developer with passion for the frontend.
+            Enjoys working with React, TypeScript and JavaScript technologies. 
+            You will also find me using Tailwind CSS
+          </p>
+        </div>
+        <div className='text-3xl flex justify-center gap-16'>
+          <AiFillTwitterCircle />
+          <AiFillLinkedin />
+          <AiFillGithub />
+        </div>
+      </section>
     </main>
+   </div> 
+
   )
 }
