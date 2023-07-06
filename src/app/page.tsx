0d864edbf3aff6e6ec13f02ffe6f2c03e397893d
Bruce Simiyu <bruce14simiyu@gmail.com>
2023-07-06 14:24:47 +0300

    The profile picture breaks the code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,81 @@
-import {AiFillTwitterCircle, AiFillLinkedin, AiFillGithub} from 'react-icons/ai';
+import {AiFillTwitterCircle, AiFillLinkedin, AiFillGithub} from "react-icons/ai";
 import Image from "next/image"
 import profile from "../../public/profile.jpg"
 
 export default function Home() {
   return (
    <div>
-    <main className="bg-white px-10">
+    <main className="bg-black text-white px-10">
       <section className="h-screen ">
         <nav className="py-4 mb-1">
-          <h1 className='font-bold'><code>Bruce</code></h1>
+          <h1 className="font-bold"><code>Bruce</code></h1>
         </nav>
         <div className="text-center">
-          <h2 className='text-4xl py-2 text-teal-700 font-medium'>OBruce Simiyu</h2>
-          <h3 className='py-2 font-medium'>Web Developer | Technical Writer</h3>
-          <p className='leading-6 py-3 text-gray-900'>A freelance web developer with passion for the frontend.
+          <h2 className="text-4xl py-2 text-teal-700 font-medium">OBruce Simiyu</h2>
+          <h3 className="py-2 font-medium">Web Developer | Technical Writer</h3>
+          <p className="leading-6 py-3 text-red-300">A freelance web developer with passion for the frontend.
             Enjoys working with React, TypeScript and JavaScript technologies. 
             You will also find me using Tailwind CSS
           </p>
         </div>
-        <div className='text-3xl flex justify-center gap-16'>
+        <div className="text-3xl flex justify-center gap-16 py-8">
           <AiFillTwitterCircle />
           <AiFillLinkedin />
           <AiFillGithub />
         </div>
-        <div className=''>
-          <Image src={profile} alt=""/>
+        <div className="w-40 h-40 m-auto text-center">
+          <Image src={profile} alt="" className="rounded-full"/>
 
         </div>
       </section>
+      <section className="leading-6 ">
+        <h2 className="font-extrabold text-3xl">About</h2>
+        <h1>I am a full stack software craftsman with a soft spot for the frontend.
+          I use my curiosity, obsessive attention to detail, unequivocal love for 
+          creation of things and a mission-driven work ethic to literally extend the boundaries
+          of whats possible in the world of technology.
+        <br />
+        I have a working proficiency in JavaScript, TypeScript, React/Next, Tailwind CSS, and Styled Components
+          I focus primarily on delivering clean code that is easy to maintain and scale using Test Driven Development. <br /><br />
+          Additionally, I specialize in creating concise, detailed and accurate documents that helps developers comprehend
+          even the most complex systems and topics. I have experience in creating user manuals, software documents, and technical reports.<br /><br />
+          Away from work, when the screens go off, I extend my creativity continues to flow as a quadbike speed junkie, and a cueist. <br /><br />
+          Here is my technology toolbelt:
+           </h1> 
+        <ul className="text-sm mx-auto text-center">
+          <li className="">
+              <li className="py-1 my-2">
+                  <span className="bg-blue-600 rounded p-1 mx-2">JavaScript</span>
+                  <span className="bg-blue-600 rounded p-1 mx-2">Ruby</span>
+                  <span className="bg-blue-600 rounded p-1 mx-2">Tailwind CSS</span>
+                  <span className="bg-blue-600 rounded p-1 mx-2">React/Redux</span><br />
+              </li>
+              <li className="my-2 py-1">    
+                  <span className="bg-blue-600 rounded p-1 mx-2">Next</span>
+                  <span className="bg-blue-600 rounded p-1 mx-2">TypeScript</span>
+                  <span className="bg-blue-600 rounded p-1 mx-2">ShadCN</span>
+                  <span className="bg-blue-600 rounded p-1 mx-2">AWS</span>
+                  <span className="bg-blue-600 rounded p-1 mx-2">GraphQL</span><br />
+                  
+              </li> 
+            
+              <li className="my-2 py-1">
+                  <span className="bg-blue-600 rounded p-1 mx-2">Vitest</span>
+                  <span className="bg-blue-600 rounded p-1 mx-2">GitHub Actions</span>
+                  <span className="bg-blue-600 rounded p-1 mx-2">Git/GitHub</span>
+                  <span className="bg-blue-600 rounded p-1 mx-2">RESTful API</span><br />
+              </li>
+          </li>
+        </ul>   
+        
+
+      </section>
+      <footer className="">
+        <div className="flex text-sm list-none gap-1">
+            <li>Dark |</li>
+            <li>Light</li>
+        </div>
+      </footer>
     </main>
    </div> 
 
